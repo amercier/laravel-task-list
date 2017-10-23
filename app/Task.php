d6863cf3915e8535fe3985211b37d8f11d01529d
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-10-23 11:03:19 +0200

    Set Task::name as mass assignable

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = ['name'];
 }
