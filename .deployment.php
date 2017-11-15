7ef49a5d53f7df6ef5484293a80e54bd7bc71412
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-15 15:24:26 +0100

    Replaced .webremote.php by amercier/webremote-cli

--- .deployment.php
+++ .deployment.php
@@ -4,7 +4,7 @@ return [
     'laravel-task-list.amercier.com' => [
         'remote' => 'ftp://'
             . getenv('DEPLOY_FTP_USERNAME') . ':' . getenv('DEPLOY_FTP_PASSWORD')
-            . '@ftp.amercier.com/' . preg_replace('/^\//', '', getenv('DEPLOY_PATH')),
+            . '@ftp.amercier.com/' . getenv('DEPLOY_PATH'),
         'local' => '.',
         'ignore' => file_get_contents('.deployignore'),
         'allowDelete' => true,
